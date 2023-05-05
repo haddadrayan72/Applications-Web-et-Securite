b3713695ddb3b00fbac6eb9f374ab419608b76e8
uvsq21916859 <marculiosage@gmail.com>
2023-05-05 18:36:12 +0200

    ajout de programme

---
+++ fitlearning/src/Pages/PertePoid.js
@@ -0,0 +1,117 @@
+import React from "react";
+import './PertePoid.css';
+
+const PertePoid = () => {
+return (
+
+<div>
+<h1>Programme perte de poids :</h1>
+<h2>Objectif</h2>
+<p>
+L'objectif de ce programme d'entraînement est de vous aider à perdre du
+poids en brûlant des calories à travers l'exercice et en modifiant votre
+alimentation. L'objectif final est de vous aider à atteindre un poids
+santé et de vous aider à maintenir votre poids idéal à long terme.
+</p>
+<h2>Échauffement</h2>
+<p>
+Un échauffement est important pour préparer votre corps à l'exercice et
+éviter les blessures. Voici un exemple d'échauffement que vous pouvez
+faire avant chaque séance d'entraînement :
+</p>
+<ol>
+<li>
+Marche rapide pendant 5 minutes pour augmenter votre rythme cardiaque
+</li>
+<li>
+Étirements dynamiques pour échauffer les muscles que vous allez
+utiliser pendant l'exercice, tels que les squats, les fentes, les
+jumping jacks, les burpees, etc.
+</li>
+<li>
+Faites une série de 10 à 15 répétitions de chaque exercice que vous
+allez faire pendant votre séance d'entraînement avec des poids légers
+pour échauffer vos muscles.
+</li>
+</ol>
+<h2>Séance d'entraînement</h2>
+<p>
+Le programme de perte de poids est basé sur un programme de cardio et de
+musculation. L'objectif est de brûler des calories et de tonifier les
+muscles pour améliorer votre composition corporelle et perdre du poids.
+Voici le programme :
+</p>
+<h3>Jour 1 : Entraînement cardio</h3>
+<ul>
+<li>
+Marche rapide sur tapis roulant ou à l'extérieur pendant 30 minutes
+</li>
+<li>
+Vélo elliptique ou rameur pendant 20 minutes
+</li>
+<li>
+Étirements statiques pour les muscles sollicités pendant l'exercice
+</li>
+</ul>
+<h3>Jour 2 : Musculation (haut du corps)</h3>
+<ul>
+<li>Échauffement de 5 à 10 minutes</li>
+<li>3 séries de 10 répétitions de <a href="https://www.youtube.com/shorts/7P562roxtSc">développé couché</a></li>
+  <li>3 séries de 10 répétitions de <a href="https://www.youtube.com/shorts/IcE5c6qf4KE">tirage à la poulie haute</a></li>
+<li>3 séries de 10 répétitions de <a href="https://www.youtube.com/shorts/5pjcqP_nqRA">développé militaire</a></li>
+<li>3 séries de 10 répétitions de <a href="https://www.youtube.com/shorts/_xC3qsF9cO4">triceps barre au front</a></li>
+</ul>
+<h3>Jour 3 : Entraînement cardio</h3>
+<ul>
+<li>
+Course à pied sur tapis
+</li>
+        <li>
+          Escalade sur machine à escalier ou course en montée pendant 15 minutes
+        </li>
+        <li>
+          Étirements statiques pour les muscles sollicités pendant l'exercice
+        </li>
+      </ul>
+      <h3>Jour 4 : Repos</h3>
+      <p>
+        Il est important de prendre un jour de repos pour permettre à votre corps
+        de récupérer et de se préparer pour la suite de la semaine.
+      </p>
+      <h3>Jour 5 : Musculation (bas du corps)</h3>
+      <ul>
+        <li>Échauffement de 5 à 10 minutes</li>
+        <li>3 séries de 10 répétitions de squat</li>
+        <li>3 séries de 10 répétitions de soulevé de terre</li>
+        <li>3 séries de 10 répétitions de fentes avant</li>
+        <li>3 séries de 10 répétitions de leg curls</li>
+      </ul>
+      <h3>Jour 6 : Entraînement cardio</h3>
+      <ul>
+        <li>
+          Marche rapide sur tapis roulant ou à l'extérieur pendant 30 minutes
+        </li>
+        <li>
+          Vélo elliptique ou rameur pendant 20 minutes
+        </li>
+        <li>
+          Étirements statiques pour les muscles sollicités pendant l'exercice
+        </li>
+      </ul>
+      <h3>Jour 7 : Repos</h3>
+      <p>
+        Encore une fois, prenez un jour de repos pour permettre à votre corps de
+        récupérer et de se préparer pour la prochaine semaine d'entraînement.
+      </p>
+      <h2>Conclusion</h2>
+      <p>
+        En suivant ce programme de perte de poids, vous devriez être en mesure
+        de brûler des calories, de tonifier vos muscles et d'atteindre un poids
+        santé. Assurez-vous de consulter votre médecin avant de commencer tout
+        programme d'entraînement ou de modification de votre alimentation.
+      </p>
+    </div>
+  );
+};
+
+export default PertePoid;
---
+++ fitlearning/src/Pages/PriseMass.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './PriseMass.css';
+
+const PriseMass = () =>  {
+  return (
+    <div>
+      <h2>Programme d’entraînement spécialisé prise de masse :</h2>
+
+      <h3>Objectif :</h3>
+      <p>
+        L'objectif de ce programme d'entraînement est d'augmenter votre masse musculaire de manière saine et progressive. En combinant une alimentation équilibrée avec des exercices de musculation appropriés, vous pourrez augmenter votre force et votre endurance tout en atteignant votre objectif de prendre de la masse musculaire.
+      </p>
+
+      <h3>Echauffement :</h3>
+      <p>
+        Un échauffement est important pour préparer votre corps à l'exercice et éviter les blessures. Voici un exemple d'échauffement que vous pouvez faire avant chaque séance d'entraînement :
+      </p>
+      <ol>
+        <li>Marche rapide pendant 5 minutes pour augmenter votre rythme cardiaque</li>
+        <li>Étirements dynamiques pour échauffer les muscles que vous allez utiliser pendant l'exercice, tels que les squats, les fentes, les jumping jacks, les burpees, etc.</li>
+        <li>Faites une série de 10 à 15 répétitions de chaque exercice que vous allez faire pendant votre séance d'entraînement avec des poids légers pour échauffer vos muscles.</li>
+      </ol>
+
+      <h3>Programme Push-Pull-Jambes</h3>
+      <p>
+        Le programme "push-pull-jambes" est un programme d'entraînement qui se concentre sur les mouvements de poussée, les mouvements de traction et les exercices pour les jambes. Cette méthode d'entraînement est très efficace pour augmenter la masse musculaire, améliorer la force et la stabilité des articulations, et augmenter l'endurance et la flexibilité.
+      </p>
+      <h4>Jour 1 : Push</h4>
+      <ul>
+        <li>Développé couché avec haltères</li>
+        <li>Développé incliné avec haltères</li>
+        <li>Dips</li>
+        <li>Développé militaire avec haltères</li>
+        <li>Écarté incliné avec haltères</li>
+      </ul>
+      <h4>Jour 2 : Pull</h4>
+      <ul>
+        <li>Tirage horizontal avec poulie</li>
+        <li>Tirage vertical à la poulie haute</li>
+        <li>Tractions assisté avec banc</li>
+        <li>Rowing avec barre</li>
+        <li>Curl biceps avec barre</li>
+      </ul>
+      <h4>Jour 3 : Jambes</h4>
+      <ul>
+        <li>Squats avec barre</li>
+        <li>Fentes arrière</li>
+        <li>Extensions mollets</li>
+        <li>Presse à cuisses</li>
+      </ul>
+
+      <h3>Nombre de séries et de répétitions</h3>
+      <p>Pour chaque exercice, vous devriez effectuer 3 à à 4 séries de 8 à 12 répétitions, avec un poids qui vous permet de compléter toutes les répétitions de manière appropriée tout en vous poussant à la limite. Assurez-vous de bien vous reposer entre chaque série, environ 1 à 2 minutes, pour permettre à vos muscles de récupérer avant de passer à la série suivante.</p>
+      <h3>Alimentation pour prise de masse</h3>
+  <p>Pour prendre de la masse musculaire, il est important d'adopter une alimentation saine et équilibrée, riche en protéines, glucides et graisses saines. Voici quelques conseils pour une alimentation appropriée pour la prise de masse :</p>
+  <ul>
+    <li>Assurez-vous de consommer suffisamment de calories pour répondre à vos besoins énergétiques élevés en raison de l'exercice régulier.</li>
+    <li>Mangez des protéines de haute qualité à chaque repas, comme des viandes maigres, des œufs, du poisson, des légumineuses et des produits laitiers.</li>
+    <li>Choisissez des glucides complexes comme les grains entiers, les fruits, les légumes et les légumineuses pour fournir de l'énergie et des nutriments essentiels pour les muscles.</li>
+    <li>Ne négligez pas les graisses saines comme les noix, les graines, l'huile d'olive et les avocats, qui sont importantes pour la santé globale et la fonction musculaire.</li>
+    <li>Consommez suffisamment d'eau pour rester hydraté tout au long de la journée.</li>
+  </ul>
+
+  <h3>Conclusion</h3>
+  <p>Adopter une alimentation saine et équilibrée est essentiel pour atteindre votre objectif de prendre de la masse musculaire. En combinant une alimentation saine avec votre programme d'entraînement pour prendre de la masse, vous pourrez obtenir des résultats optimaux.</p>
+  <h3>Alimentation pour prise de masse</h3>
+  <p>Pour prendre de la masse musculaire, il est important d'adopter une alimentation saine et équilibrée, riche en protéines, glucides et graisses saines. Voici quelques conseils pour une alimentation appropriée pour la prise de masse :</p>
+  <ul>
+    <li>Assurez-vous de consommer suffisamment de calories pour répondre à vos besoins énergétiques élevés en raison de l'exercice régulier.</li>
+    <li>Mangez des protéines de haute qualité à chaque repas, comme des viandes maigres, des œufs, du poisson, des légumineuses et des produits laitiers.</li>
+    <li>Choisissez des glucides complexes comme les grains entiers, les fruits, les légumes et les légumineuses pour fournir de l'énergie et des nutriments essentiels pour les muscles.</li>
+    <li>Ne négligez pas les graisses saines comme les noix, les graines, l'huile d'olive et les avocats, qui sont importantes pour la santé globale et la fonction musculaire.</li>
+    <li>Consommez suffisamment d'eau pour rester hydraté tout au long de la journée.</li>
+  </ul>
+
+  <h3>Conclusion</h3>
+  <p>Adopter une alimentation saine et équilibrée est essentiel pour atteindre votre objectif de prendre de la masse musculaire. En combinant une alimentation saine avec votre programme d'entraînement pour prendre de la masse, vous pourrez obtenir des résultats optimaux.</p>
+</div>
+);
+}
+
+export default PriseMass;
+
